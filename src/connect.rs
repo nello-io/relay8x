477ae5c4a567403e98b5d8b2fe853c380ab13965
Korbinian Maier <korbinian.maier@tum.de>
2018-06-05 11:51:32 +0200

    fix/connect: fixed issue with non-sized struct member

--- src/connect.rs
+++ src/connect.rs
@@ -1,32 +1,32 @@
 use serial::prelude::*;
-use serial;
-//use serial_unix::*;
 use std::io;
 use std::time::Duration;
 use bytes::{BytesMut, BufMut};
+use std::rc::Rc;
 
 pub struct Relay8x {
     device_name: String,
     address: u8,
-    port: SerialPort,
+    port: Rc<SerialPort>,
 }
 
 impl Relay8x {
 
     /// constructor for a new Relay Card
-    pub fn new(device_name: String, address: u8) -> Self {
+    pub fn new(device_name: String, address: u8) -> io::Result<Self> {
         let device_name = format!("/dev/{}", device_name);
-        Self {
-            port: serial::open(&device_name).unwrap(),
+        Ok(Self {
+            port: Rc::new(::serial::open(&device_name)?),
             device_name: device_name,
             address: address,
-        }
+        })
     }
 
     /// initialise device with correct params
     /// sets device address, function can be used to re-set it
-    pub fn init_device(&self) -> io::Result<()> {
+    pub fn init_device(&mut self) -> io::Result<()> {
         self.configure_port()?;
+        let port = Rc::get_mut(&mut self.port).unwrap();
         // init relaycard
         let mut cmd = BytesMut::with_capacity(4);
         let cmd_no = 1; // first byte: command init device
@@ -35,9 +35,9 @@ impl Relay8x {
         cmd.put_u8(0);  // third: dont care
         cmd.put_u8(cmd_no ^ self.address ^ 0); // fourth: XOR
 
-        self.port.write(&cmd[..])?;
+        port.write(&cmd[..])?;
 
-        self.port.read(&mut cmd[..])?;
+        port.read(&mut cmd[..])?;
         debug!("Response: {:?}", cmd);
         // TODO return response and check if ok
 
@@ -45,18 +45,19 @@ impl Relay8x {
     }
 
     /// private function for port settings
-    fn configure_port(&self) -> io::Result<()> {
-        
-        self.port.reconfigure(&|settings| {
-            settings.set_baud_rate(serial::Baud19200)?;
-            settings.set_char_size(serial::Bits8);
-            settings.set_parity(serial::ParityNone);
-            settings.set_stop_bits(serial::Stop1);
-            settings.set_flow_control(serial::FlowNone);
+    fn configure_port(&mut self) -> io::Result<()> {
+        let port = Rc::get_mut(&mut self.port).unwrap();
+
+        port.reconfigure(&|settings| {
+            settings.set_baud_rate(::serial::Baud19200)?;
+            settings.set_char_size(::serial::Bits8);
+            settings.set_parity(::serial::ParityNone);
+            settings.set_stop_bits(::serial::Stop1);
+            settings.set_flow_control(::serial::FlowNone);
             Ok(())
         })?;
 
-        self.port.set_timeout(Duration::from_millis(1000))?;
+        port.set_timeout(Duration::from_millis(1000))?;
 
         Ok(())
     }
@@ -64,7 +65,9 @@ impl Relay8x {
     /// switch one relay on or off
     /// number: 1..8 corresponding to relays X1 to X8 on the board
     /// state: true for switching on, false for off
-    pub fn set_relay(&self, number: u8, state: bool) -> io::Result<()> {
+    pub fn set_relay(&mut self, number: u8, state: bool) -> io::Result<()> {
+        let port = Rc::get_mut(&mut self.port).unwrap();
+
         // cmd message has 4 bytes
         let mut cmd = BytesMut::with_capacity(4);
         let on_off = if state { // on
@@ -77,7 +80,7 @@ impl Relay8x {
         cmd.put_u8(number);
         cmd.put_u8(on_off ^ self.address ^ number);
 
-        self.port.write(&cmd[..])?;
+        port.write(&cmd[..])?;
         // TODO check response
 
         Ok(())
@@ -86,9 +89,11 @@ impl Relay8x {
     /// switch more than one relay on or off
     /// numbers: Vector containing all relay numbers (1..8)
     /// state; true for switching on, false for off
-    pub fn set_relays(&self, numbers: Vec<u8>, state: bool) -> io::Result<()> {
-         let mut cmd = BytesMut::with_capacity(4);
-         let on_off = if state { // on
+    pub fn set_relays(&mut self, numbers: Vec<u8>, state: bool) -> io::Result<()> {
+        let port = Rc::get_mut(&mut self.port).unwrap();
+        
+        let mut cmd = BytesMut::with_capacity(4);
+        let on_off = if state { // on
             6
         } else { // off
             7
@@ -104,13 +109,16 @@ impl Relay8x {
 
         println!("{:?} => {:b}", numbers, relay_bin);
 
-        self.port.write(&cmd[..])?;
+
+        port.write(&cmd[..])?;
         // TODO check the repsonse
 
         Ok(())
     }
 
-    pub fn toggle_relays(&self, numbers: Vec<u8>) -> io::Result<()> {
+    pub fn toggle_relays(&mut self, numbers: Vec<u8>) -> io::Result<()> {
+        let port = Rc::get_mut(&mut self.port).unwrap();
+        
         let mut cmd = BytesMut::with_capacity(4);
         // toggle is command no 8
         cmd.put_u8(8); 
@@ -122,7 +130,7 @@ impl Relay8x {
         cmd.put_u8(relay_bin);
         cmd.put_u8(8 ^ self.address ^ relay_bin);
 
-        self.port.write(&cmd[..])?;
+        port.write(&cmd[..])?;
         // check the response
 
         Ok(())
