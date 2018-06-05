252671b793c3fc515996d2ed5017632d9d67e928
Korbinian Maier <korbinian.maier@tum.de>
2018-06-05 10:53:41 +0200

    feat/*: added toogle functionality and docopt handling in main

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,7 @@ extern crate bytes;
 #[macro_use]
 extern crate log;
 
-use serial::prelude::*;
 use docopt::Docopt;
-use std::io;
-use bytes::{BytesMut, BufMut};
 
 mod connect;
 use connect::*;
@@ -21,15 +18,16 @@ const USAGE: &'static str = "
 relais8x
 
 Usage:
-  relais8x set --dev=<dev> [--relay=<relay>] on
-  relais8x set --dev=<dev> [--relay=<relay>] off
-  relais8x reset --dev=<dev>
+  relais8x set --dev=<dev> [--relay=<relay>] <state>
+  relais8x toggle --dev=<dev> [--relay=<relay>]
+  relais8x reset --dev=<dev> [--relay=<relay>]
   relais8x (-h | --help)
   relais8x (-v | --version)
   
 Commands:
-  set   set specified relay on or off, if no relay number is given all relays are set
+  set   set specified relay 'on' or 'off', if no relay number is given all relays are set
   toggle    toggle specified relay,  if no relay number is given all relays are toggeled
+  reset switch all or just one relay off to reach defined state again
 
 Options:
   -h --help     Show this screen.
@@ -42,10 +40,12 @@ Options:
 struct Args {
     cmd_set: bool,
     cmd_toggle: bool,
+    cmd_reset: bool,
     flag_dev: String,
-    flag_relay: u8,
+    flag_relay: Option<Vec<u8>>,
     flag_version: bool,
     flag_help: bool,
+    arg_state: String,
 }
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -68,8 +68,48 @@ fn main() {
         // open device, address of relay is always 1 as for now
         let relay = Relay8x::new(args.flag_dev, 1);
         relay.init_device();
+        // map state argument to bool, use false as default
+        let state = match args.arg_state.as_ref() {
+            "on" => true,
+            "off" => false,
+            _ => { 
+                println!("Failed to determine state '{}', used off", args.arg_state);
+                false }
+        };
+        // if flag is none, all relays should be set
+        let relay_numbers = if args.flag_relay.is_none() {
+            vec![1,2,3,4,5,6,7,8]
+        } else {
+            args.flag_relay.unwrap()
+        };
+        // do the switching
+        relay.set_relays(relay_numbers, state);
 
-
+    } else if args.cmd_toggle {
+        // open device
+        let relay = Relay8x::new(args.flag_dev, 1);
+        relay.init_device();
+        // if flag is none, all relays should be toggeled
+        let relay_numbers = if args.flag_relay.is_none() {
+            vec![1,2,3,4,5,6,7,8]
+        } else {
+            args.flag_relay.unwrap()
+        };
+        // do the toggle
+        relay.toggle_relays(relay_numbers);
+    } else if args.cmd_reset {
+        // open device
+        let relay = Relay8x::new(args.flag_dev, 1);
+        relay.init_device();
+        // if flag is none, all relays should be reset
+        let relay_numbers = if args.flag_relay.is_none() {
+            vec![1,2,3,4,5,6,7,8]
+        } else {
+            args.flag_relay.unwrap()
+        };
+        // do the switching
+        relay.set_relays(relay_numbers, false);
     }
 }
+
 
